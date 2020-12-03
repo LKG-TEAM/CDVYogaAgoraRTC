eaa075c6adbcbc140dcf8e1760567d17f0878cae
pantao <771764208@qq.com>
2020-12-03 14:28:08 +0800

    Update YogaAgoraShared.m

--- src/ios/YogaAgoraShared.m
+++ src/ios/YogaAgoraShared.m
@@ -29,35 +29,8 @@ static dispatch_once_t onceToken;
 - (instancetype)init
 {
     if (self = [super init]) {
-        UIDevice *device = [UIDevice currentDevice]; //Get the device object
-            [device beginGeneratingDeviceOrientationNotifications]; //Tell it to start monitoring the accelerometer for orientation
-        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(deviceOrientationListener:) name:UIDeviceOrientationDidChangeNotification object:device];
     }
     return self;
-}
-
-- (void)deviceOrientationListener:(NSNotification *)n
-{
-    UIDeviceOrientation o = [[UIDevice currentDevice] orientation];
-    switch (o) {
-        case UIDeviceOrientationPortrait:
-            NSLog(@"竖屏");
-            break;
-        case UIDeviceOrientationPortraitUpsideDown:
-            NSLog(@"倒屏");
-            break;
-        case UIDeviceOrientationLandscapeLeft:
-//            [[UIApplication sharedApplication] setStatusBarOrientation:UIInterfaceOrientationLandscapeRight animated:YES];
-            NSLog(@"左屏");
-            break;
-        case UIDeviceOrientationLandscapeRight:
-//            [[UIApplication sharedApplication] setStatusBarOrientation:UIInterfaceOrientationLandscapeLeft animated:YES];
-            NSLog(@"右屏");
-            break;
-        default:
-            break;
-    }
-    [[YogaAgoraShared shared].commandDelegate evalJs:[YogaAgoraUtil jsFuncFormat:[NSString stringWithFormat:@"DeviceOrientation(%ld)",o]]];
 }
 
 - (NSMutableDictionary *)commands
